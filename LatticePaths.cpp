4b4227f1fcd79f133a68d3ee01c21d5121e5eebd
jdaniel400 <jasondaniel95@gmail.com>
2016-06-18 11:30:50 -0500

    finished lattice paths with dp

--- LatticePaths.cpp
+++ LatticePaths.cpp
@@ -6,39 +6,40 @@
 using namespace std;
 
 
+int findPaths (int **paths, int rows, int cols) {
 
-
-int findPaths_helper (int * paths, int rows, int cols)
-{
-    int down_paths, right_paths;
-    if (paths[rows-1][cols] != 0)
-        right_paths = paths[rows-1][cols];
-    else
-        findPaths_helper (paths, rows - 1, cols);
-    if (paths[rows][cols-1] != 0)
-        down_paths = paths[rows][cols-1];
-    else
-        findPaths_helper (paths, rows, cols - 1);
-    return down_paths + right_paths;
-
+  return paths[rows][cols]; // no need for recursion
 }
-void findPaths (int rows, int cols) {
-  int paths [500][500];
-  paths[0][1] = 1;
+
+int **build_paths_matrix ()
+{
+    //build 2D array to store number of paths for each lattice dimensions
+  int **paths = new int* [500];
+  for (int i = 0; i < 500; i++)
+      paths[i] = new int [500];
+  for (int i = 0; i < 500; i++) {
+      paths[i][0] = 1; //intialize the top and left borders to 1's
+      paths[0][i] = 1;
+  }
+  paths[0][1] = 1; //start off the smallest two sizes
   paths[1][0] = 1;
-  findPaths_helper (paths, rows, cols);
+
+  for (int i = 1; i < 500; i++)
+    for (int j = 1; j < 500; j++)
+      paths[i][j] = (paths[i-1][j] + paths[i][j-1]) % 1000000007; //dynamic programming solution
+  return paths;
 }
 
 int main() {
     int d, m, n;
-    cin >> d;
-    int sum;
-   	for (int i = 0; i < d; i++) {
-        sum = 0;
-   		cin >> n;
-   		cin >> m;
-      cout << findPaths (n, m) << endl;
-   	}
+    int **paths = build_paths_matrix ();
+    cin >> d; //get number of trials
+    for (int i = 0; i < d; i++) {
+      cin >> n; //number of rows
+      cin >> m; //number of columns
+      cout << findPaths (paths, n, m) << endl; //return answer
+    }
 
     return 0;
 }
+
